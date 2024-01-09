c53ef61385567d0318e61a1f9d5d889532c3ba40
Bastien GUILLOU <bastien.guillou@edu.esiee.fr>
2024-01-09 14:46:41 +0100

    Automatisation_migration_donnees_PostGresSQL_a_ElasticSearch

--- run_elastic_migrator.sh
+++ run_elastic_migrator.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # Attendre que le conteneur scrapy s'arrête
 echo "En attente de la fin du conteneur scrapy..."
@@ -6,7 +5,6 @@ while [ $(docker ps -f name=scrapy-f status=running -q | wc -l) -gt 0 ]; do
     sleep 1
 done
 
-# Attendre 5 secondes supplémentaires
 echo "Attente de 5 secondes..."
 sleep 5
 
